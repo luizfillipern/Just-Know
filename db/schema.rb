4cf568d87ef42ffac85a5d52f456d307c5938b24
Davyson Costa <davyson59@gmail.com>
2011-11-01 20:59:45 -0300

    added attr and fixed something of user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111031220057) do
+ActiveRecord::Schema.define(:version => 20111101185909) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(:version => 20111031220057) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.string   "name"
+    t.text     "about_me"
   end
 
   add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
